c25b0d029da0ea98ed3c9ee20d21c4bf2806908e
redstarbird <contact@redstarbird.com>
2026-04-23 18:51:24 +0100

    Added sizing helper macros to MemoryArena.h

--- include/MemoryArena.h
+++ include/MemoryArena.h
@@ -9,4 +9,13 @@ struct MemoryArena *CreateArena(size_t size);
 void *arenaAllocAlign(struct MemoryArena *arena, size_t size, size_t alignment);
 void DestroyArena(struct MemoryArena *arena);
 
+// ------
+// Macros
+// ------
+
+// Sizing helper macros
+#define KB(x) ((size_t)(x) * 1024)
+#define MB(x) (KB(x) * 1024)
+#define GB(x) (MB(x) * 1024)
+
 #endif // MEMORYARENA_H
