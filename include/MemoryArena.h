f3cbc8c51c4f83831f06b391b7ab970947de3f71
redstarbird <contact@redstarbird.com>
2026-05-11 14:02:37 +0100

    Add TB macro for terabyte size calculations in MemoryArena

--- include/MemoryArena.h
+++ include/MemoryArena.h
@@ -42,6 +42,7 @@ void EndTempArena(struct TempArena temp);
 #define KB(x) ((size_t)(x) * 1024)
 #define MB(x) (KB(x) * 1024)
 #define GB(x) (MB(x) * 1024)
+#define TB(x) (GB(x) * 1024)
 
 // Type safe allocation macros
 
