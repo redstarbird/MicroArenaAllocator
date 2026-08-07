13267947884b4ff9d32572d8ad541c57448b975b
redstarbird <contact@redstarbird.com>
2026-08-07 10:51:39 +0100

    Fix bug with duplicated header guard in generated single-header release file

--- scripts/generate_header_release_files.py
+++ scripts/generate_header_release_files.py
@@ -71,10 +71,8 @@ def generate():
         f.write("/*In one C/C++ file, do: #define MICRO_ARENA_IMPLEMENTATION*/\n")
 
         # Header content
-        f.write("\n\n\n#ifndef MICROARENA_H\n")
-        f.write("#define MICROARENA_H\n")
         f.write(header_content)
-        f.write("#endif // MICROARENA_H")
+
 
         f.write("\n\n/* ========================================================= */\n")
         f.write("/* IMPLEMENTATION */\n")
